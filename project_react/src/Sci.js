672a11e132df468c26bad44d07ea2463652344eb
AmyStas <95294347+amystas@users.noreply.github.com>
2024-10-02 09:24:37 +0200

    references, arrow functions

--- project_react/src/Sci.js
+++ project_react/src/Sci.js
@@ -1,5 +1,20 @@
+import { useRef } from "react";
+
 function Sci() {
-    return <div>SCI</div>;
+    const inputRef = useRef();
+    const handleButton = () => {
+        console.log("hello world");
+        console.log(inputRef.current.value);
+    } 
+    const handleInputText = (event) => {
+        console.log("text input", event.target.value);
+    }
+    return( <> 
+        <div>
+            <input type="button" value="SCI button" onClick={handleButton}></input>
+            <input ref={inputRef} type="text" onChange={handleInputText}></input>
+        </div>
+    </>);
 }
 
 export default Sci;
